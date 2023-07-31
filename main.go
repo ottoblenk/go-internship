bfd55499c264505e5a0f479dabc7c4ccbaa914a9
otto.blenk <otto.blenk@blacklane.com>
2023-07-31 15:42:51 +0200

    picture not work

--- main.go
+++ main.go
@@ -6,17 +6,39 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!")
-	})
-
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1> we are home </h1>")
-	})
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "src/templates/pages/home.html")
 }
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	fmt.Fprint(w, "<h1>About Us</h1><p>This is our website!</p>")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	fmt.Println("Starting server at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// func main() {
+// 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "Hello!")
+// 	})
+//
+// 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "<h1> we are not home </h1>")
+// 	})
+//
+// 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "<h1> we are not home </h1>")
+// 	})
+//
+// 	fmt.Printf("Starting server at port 8080\n")
+// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
