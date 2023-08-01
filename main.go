8584aa9b6cfdfe370b09f39ccf4e7b5f2cb4163b
Otto Blenk <otto.blenk@blacklane>
2023-08-01 13:22:28 +0200

    navigation addes but bad gui

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/example", exampleHandler)
 
-	fmt.Println("Starting server at http://localhost:8090")
+	fmt.Println("Starting home at http://localhost:8090")
+	fmt.Println("Starting about at http://localhost:8090/about")
+	fmt.Println("Starting contact at http://localhost:8090/contact")
+	fmt.Println("https://localhost:8090/example")
 	log.Fatal(http.ListenAndServe(":8090", nil))
 }
 
@@ -20,9 +24,13 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/teplates/pages/contact.html")
+	http.ServeFile(w, r, "src/templates/pages/about.html")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/teplates/pages/contact.html")
+	http.ServeFile(w, r, "src/templates/pages/contact.html")
+}
+
+func exampleHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "src/templates/pages/example.html")
 }
