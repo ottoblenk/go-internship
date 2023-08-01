baaec0c5174936b42c53b174d6f6494335144f9e
Otto Blenk <otto.blenk@blacklane>
2023-08-01 16:40:56 +0200

    progress but still errors

--- main.go
+++ main.go
@@ -1,61 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	// Define the data for the templates (e.g., title and year)
-	// Define the data for the templates (e.g., title and year)
-	data := map[string]string{
-		"Title": "My Website",
-		"Year":  "2023",
-	}
+	// file paths for the templates
+	baseTemplatePath := filepath.Join("src", "templates", "base.html")
+	homeTemplatePath := filepath.Join("src", "templates", "pages", "home.html")
+	contactTemplatePath := filepath.Join("src", "templates", "pages", "contact.html") // Add a new Template like this:
+	aboutTemplatePath := filepath.Join("src", "templates", "pages", "about.html")     // xyzTemplatePath := filepath.Join("src", "templates", "pages", "xyz.html")
 
-	// Load the base template and parse child templates
-	templates := template.Must(template.ParseFiles("base.html", "home.html", "contact.html", "about.html", "example.html"))
+	// Load the base template and parse the templates
+	templates := template.Must(template.ParseFiles(baseTemplatePath, homeTemplatePath, contactTemplatePath, aboutTemplatePath)) // Add the xyzTemplatePath here
 
-	// Handle requests using a handler function
-	http.HandleFunc("/page1", func(w http.ResponseWriter, r *http.Request) {
-		// Execute child_template1.html using the base template and data
-		templates.ExecuteTemplate(w, "base_template.html", data)
+	// Home Page: http://localhost:8090/
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "base.html", nil)
 	})
 
-	http.HandleFunc("/page2", func(w http.ResponseWriter, r *http.Request) {
-		// Execute child_template2.html using the base template and data
-		templates.ExecuteTemplate(w, "base_template.html", data)
+	// Contact Page: http://localhost:8090/contact
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "base.html", nil)
 	})
+
+	// About Page: http://localhost:8090/about
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "base.html", nil)
+	})
+
+	// 		New Page: http://localhost:8090/xyz
+	//	http.HandleFunc("/xyz", func(w http.ResponseWriter, r *http.Request) {
+	//		templates.ExecuteTemplate(w, "base.html", nil)
+	//	})
 
 	// Start the server
-	http.ListenAndServe(":8080", nil)
-
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/contact", contactHandler)
-	http.HandleFunc("/example", exampleHandler)
-
-	fmt.Println("Starting home at http://localhost:8090")
-	fmt.Println("Starting about at http://localhost:8090/about")
-	fmt.Println("Starting contact at http://localhost:8090/contact")
-	fmt.Println("https://localhost:8090/example")
-	log.Fatal(http.ListenAndServe(":8090", nil))
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/templates/pages/home.html")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/templates/pages/about.html")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/templates/pages/contact.html")
-}
-
-func exampleHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "src/templates/pages/example.html")
+	http.ListenAndServe(":8090", nil)
 }
